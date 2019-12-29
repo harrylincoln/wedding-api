8d40188880c4bc5343ed256cb8e575e655964871
harrylincoln <harrylincoln0612@gmail.com>
2019-12-29 12:29:03 -0300

    correct iam/aws config

--- handler.js
+++ handler.js
@@ -1,3 +1,6 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 export const hello = async (event, context) => {
   return {
     statusCode: 200,
@@ -5,6 +8,35 @@ export const hello = async (event, context) => {
       message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
     }),
   };
+};
+
+export const getInvitation = (event, context, callback) => {
+  const params = {
+    TableName: 'invitation',
+    Key: {
+      invitation_id: 'someInvitation',
+    },
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
 };
 
 const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
